95e4500ee374ed5383596e0a8eef52827c5083a5
Aman Kumar Singh <singhamankr17@gmail.com>
2025-07-14 22:02:35 +0530

    turbo_play

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Gradient } from "@repo/ui/gradient";
+import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+
+
+
+export default function Page() {
+  return (
+    <div className="text-white bg-black text-4xl">This is the main page</div>
+  );
+}
