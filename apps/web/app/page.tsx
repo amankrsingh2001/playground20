a5deca6443e7443b3b4a81e06446c8c856f710df
Aman Kumar Singh <singhamankr17@gmail.com>
2025-07-20 21:02:32 +0530

    clsx_twMerge_ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
 import { Gradient } from "@repo/ui/gradient";
 import { TurborepoLogo } from "@repo/ui/turborepo-logo";
-import Input from "@repo/ui/input";
+import  Input  from '@repo/ui/input';
+
+
+
+
 
 
 
 export default function Page() {
   return (
-    <div className="text-white bg-black text-4xl">
+    <div>
+      Hello world
         <Input/>
     </div>
   );
--- packages/ui/src/Input.tsx
+++ packages/ui/src/Input.tsx
@@ -1,3 +1,9 @@
-export default function Input(){
-    return <div></div>
+import { cn } from "../src/utils/ui";
+import { InputProps } from '@repo/interface';
+
+
+export default function Input({className}:{className?:InputProps}){
+    return <div className={cn(`bg-orange-500`, className)}>
+        <input/>
+        </div>
 }
