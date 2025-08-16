88c44f363269fadd409151767eed1622884ea4f5
Aman Kumar Singh <singhamankr17@gmail.com>
2025-08-17 00:58:19 +0530

    user_auth

--- apps/http-server/src/utils/mailSender.ts
+++ apps/http-server/src/utils/mailSender.ts
@@ -1,12 +1,25 @@
 import nodemailer from "nodemailer"
+import { ApiError } from "./ApiError"
 
 
 export const sendMail = async(email:string, title:string, body:string)=>{
-    try {
-        
+    try {   
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS
+            }
+        })
+            let info = await transporter.sendMail({
+            from:'Playground- by Aman Singh',
+            to:`${email}`,
+            subject:`${title}`,
+            html:`${body}`
+        })
+        return {success:true,data:info}
     } catch (error) {
-        
+        return {success:false,data:error}
     }
 }
-
 
